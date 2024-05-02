b95cb1c2a4d71270266cf779c3f0e15a7eb0c139
sumoduduk <sumoduduk13@gmail.com>
2024-05-02 22:52:09 +0700

    feat: add match expression for saving file to txt or json

--- src/yt_caption.rs
+++ src/yt_caption.rs
@@ -1,21 +1,54 @@
+use std::{
+    fs::File,
+    io::Write,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
 use youtube_transcript::YoutubeBuilder;
 
-pub async fn extract_srt(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn extract_srt(url: &str, format_res: &str) -> Result<(), Box<dyn std::error::Error>> {
     let yt = YoutubeBuilder::default();
     let builder = yt.build();
 
     let srt = builder.transcript(url).await?;
 
-    let caption = srt
-        .transcripts
-        .into_iter()
-        .map(|t| t.text)
-        .collect::<Vec<String>>();
+    let file_name_str = get_seconds();
 
-    let caption = caption.join(" ");
-    println!("{caption}");
+    match format_res {
+        "json" => {
+            let json = serde_json::to_string_pretty(&srt)?;
 
-    // let json = serde_json::to_string_pretty(&srt)?;
-    // println!("transcript : {}", &json);
+            let file_name = format!("results/{}.json", file_name_str);
+
+            let mut file = File::create(file_name)?;
+
+            println!("transcript : {}", &json);
+
+            file.write_all(json.as_bytes())?;
+        }
+        "text" => {
+            let caption = srt
+                .transcripts
+                .into_iter()
+                .map(|t| t.text)
+                .collect::<Vec<String>>();
+
+            let caption = caption.join(" ");
+            println!("{}", &caption);
+
+            let file_name = format!("results/{}.txt", file_name_str);
+
+            let mut file = File::create(&file_name)?;
+            file.write_all(caption.as_bytes())?;
+        }
+        _ => return Err("Need json or text".into()),
+    }
+
     Ok(())
+}
+
+fn get_seconds() -> u64 {
+    let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let seconds = current_time.as_secs();
+    seconds
 }
